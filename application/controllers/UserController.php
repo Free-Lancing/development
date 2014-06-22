56c373d85127dbb60e393bf5896044388671cf7c
maya kakda <mayakakda@gmail.com>
2014-06-22 23:45:31 +0530

    Updated User Controller

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -3,6 +3,9 @@
 class UserController extends Zend_Controller_Action {
 
     public function init() {
+        // User Init
+        $request = $this->getRequest();
+        $this->_params = $request->getParams();
     }
 
     public function indexAction() {
@@ -31,5 +34,30 @@ class UserController extends Zend_Controller_Action {
     public function cloneAction() {
         
     }
-
+    
+    public function registerAction(){
+        Zend_Layout::getMvcInstance()->setLayout('login');
+        
+    }
+    
+    public function submitAction(){
+        $objUser = new Application_Model_Users();
+        $post = $this->_request->getPost();
+        $getInfo = $objUser->getInfo();
+        
+        if(!empty($post)){
+            $checkValidation = array(
+                'first_name' => $post['fname'],
+                'last_name' => $post['lname'],
+                'email'     => $post['email'],
+                'login'     => $post['uname'],
+                'password'  => $post['password'],
+            );
+            $returnString = $this->_helper->validator($getInfo['metadata'], $checkValidation);
+            echo '<pre>';
+            print_r($returnString);
+            echo '</pre>';
+            exit;
+        }
+    }
 }
